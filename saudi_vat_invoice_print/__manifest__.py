9f080db9c421e01c83d9a22f180de7838423cda2
Aneesh AV <aneesh@amcde.com>
2021-11-11 13:58:23 +0300

    [add] modules

---
+++ saudi_vat_invoice_print/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Saudi Invoice Format',
+    'version': '1.0',
+    'depends': ['base','account'],
+    'category': 'Accounting',
+    'data': [
+        'views/res_company.xml',
+        # 'views/invoice.xml',
+        'views/res_partner.xml',
+        # 'reports/report_saudi_invoice.xml',
+     ],
+    'installable': True,
+    'application': False,
+}
